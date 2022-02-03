ea29677985d5db07ac0729cc67a8ae60f7047a4c
Vicu <vicugaideic@gmail.com>
2022-02-03 13:39:53 +0300

    table

--- src/router/index.js
+++ src/router/index.js
@@ -3,35 +3,38 @@ import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    redirect: "/home",
-  },
-  {
-    path: "/home",
-    name: "Home",
-    component: () => import("../views/Home.vue"),
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: () => import("../views/About.vue"),
-  },
-  {
-    path: "/about/:category",
-    component: () => import("../views/About.vue"),
-  },
-  {
-    path: "/table",
-    component: () => import("../views/Desserts.vue"),
-  },
+const routes = [{
+        path: "/",
+        redirect: "/home",
+    },
+    {
+        path: "/home",
+        name: "Home",
+        component: () =>
+            import ("../views/Home.vue"),
+    },
+    {
+        path: "/about",
+        name: "About",
+        component: () =>
+            import ("../views/About.vue"),
+    },
+    {
+        path: "/about/:category",
+        component: () =>
+            import ("../views/About.vue"),
+    },
+    {
+        path: "/table",
+        component: () =>
+            import ("../views/Table.vue"),
+    },
 
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  routes,
+    mode: "history",
+    routes,
 });
 
 export default router;
---
+++ src/views/Table.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+  name: "Table",
+
+  data: () => ({
+    headers: [
+          {
+            text: 'Name',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Username', value: 'username' },
+          { text: 'Address', value: 'address.city', sortable: false },
+          { text: 'Website', value: 'website' },
+          { text: 'Company', value: 'company.name' },
+          { text: 'Email', value: 'email' },
+          { text: 'Phone', value: 'phone' },
+        ],
+        items: [],
+    
+  }),
+
+  async mounted() {
+    const axios = require("axios");
+    try {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      console.log(data);
+      this.items = data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
+</script>
+
+<template>
+<div class="table">
+    <v-data-table
+    :headers="headers"
+    :items="items"
+    :items-per-page="5"
+    class="elevation-1"
+  ></v-data-table>
+</div>
+ 
+</template>
+
+<style>
+.table{
+  width: 1000px;
+  margin: 0 auto;
+}
+</style>
