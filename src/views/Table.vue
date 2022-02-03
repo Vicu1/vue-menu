1d192dd74e8de7b7cf61f783c03eed18ac48e8ad
admin <vicugaideic@gmail.com>
2022-02-03 17:41:48 +0200

    modal on table

--- src/views/Table.vue
+++ src/views/Table.vue
@@ -4,21 +4,24 @@ export default {
 
   data: () => ({
     headers: [
-          {
-            text: 'Name',
-            align: 'start',
-            sortable: false,
-            value: 'name',
-          },
-          { text: 'Username', value: 'username' },
-          { text: 'Address', value: 'address.city', sortable: false },
-          { text: 'Website', value: 'website' },
-          { text: 'Company', value: 'company.name' },
-          { text: 'Email', value: 'email' },
-          { text: 'Phone', value: 'phone' },
-        ],
-        items: [],
-    
+      {
+        text: "Name",
+        align: "start",
+        sortable: false,
+        value: "name",
+      },
+      { text: "Username", value: "username" },
+      { text: "Address", value: "address", sortable: false },
+      { text: "Website", value: "website" },
+      { text: "Company", value: "company" },
+      { text: "Email", value: "email" },
+      { text: "Phone", value: "phone" },
+    ],
+    items: [],
+    dialog: false,
+    selectedItem: {
+      address: {}
+    },
   }),
 
   async mounted() {
@@ -33,23 +36,95 @@ export default {
       console.log(error);
     }
   },
+
+  methods: {
+    openModal(item) {
+      this.dialog = true;
+      this.selectedItem = JSON.parse(JSON.stringify(item));
+    },
+  },
 };
 </script>
 
 <template>
-<div class="table">
+  <div class="table">
     <v-data-table
-    :headers="headers"
-    :items="items"
-    :items-per-page="5"
-    class="elevation-1"
-  ></v-data-table>
-</div>
- 
+      :headers="headers"
+      :items="items"
+      :items-per-page="5"
+      class="elevation-1"
+    >
+      <template #item.address="{ item }">
+        <v-tooltip bottom>
+          <template #activator="{ on, attrs }">
+            <v-chip
+              @click="openModal(item)"
+              v-bind="attrs"
+              v-on="on"
+              link
+              outlined
+            >
+              {{ item.address.city }}
+            </v-chip>
+          </template>
+          <span>
+            <div>
+              {{ item.address.street }}
+            </div>
+            <div>
+              {{ item.address.suite }}
+            </div>
+            <div>
+              {{ item.address.zipcode }}
+            </div>
+          </span>
+        </v-tooltip>
+      </template>
+      <template #item.company="{ item }">
+        {{ item.company.name}}
+      </template>
+    </v-data-table>
+    <v-dialog v-model="dialog">
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">Address</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field v-model="selectedItem.address.street" label="street" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field v-model="selectedItem.address.suite" label="suite" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field
+                  v-model="selectedItem.address.zipcode"
+                  label="zipcode"
+                  persistent-hint
+                  required
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog = false">
+            Close
+          </v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <style>
-.table{
+.table {
   width: 1000px;
   margin: 0 auto;
 }
