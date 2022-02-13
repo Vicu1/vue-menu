50da01e543b2bf2f62cb05c67e0d631654b44a4f
Vicu <vicugaideic@gmail.com>
2022-02-13 17:42:03 +0300

    old_valuues

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -8,11 +8,7 @@ export default {
   }),
 
   mounted() {
-    this.myjson.forEach(element => {
-      if (element.details.old_values['Start date'] === element.details.new_values['Start date']){
-        this.$refs.item.style.color = 'red';
-      }
-    });
+   
     
   },
 }
@@ -24,10 +20,43 @@ export default {
       <div class="old_data">
         <h1>OLD</h1>
         <div v-for="(item) of myjson" :key="item">
-          <div>
-           <p>
-             {{ item.details.old_values}} 
-            </p>
+          <div v-if="item.details.old_values">
+           <p v-if="item.details.old_values['Start date']">
+            <strong>Start date:</strong>  {{ item.details.old_values['Start date']}}; 
+          </p>
+          <p v-if="item.details.old_values['End date']">
+            <strong>End date:</strong>  {{ item.details.old_values['End date']}}; 
+          </p>
+          <p v-if="item.details.old_values['Amount'] ">
+            <strong>Amount:</strong>  {{ item.details.old_values['Amount']}}; 
+          </p>
+          <p v-if="item.details.old_values['Comment']">
+            <strong>Comment:</strong>  {{ item.details.old_values['Comment']}}; 
+          </p>
+          <p v-if="item.details.old_values['Car ID']">
+            <strong>Car ID:</strong>  {{ item.details.old_values['Car ID']}}; 
+          </p>
+          <p v-if="item.details.old_values['Collateral amount']">
+            <strong>Collateral amount:</strong>  {{ item.details.old_values['Collateral amount']}}; 
+          </p>
+          <p v-if="item.details.old_values['Address']">
+            <strong>Address:</strong>  {{ item.details.old_values['Address']}}; 
+          </p>
+          <p v-if="item.details.old_values['Client ID']">
+            <strong>Client ID:</strong>  {{ item.details.old_values['Client ID']}}; 
+          </p>
+          <p v-if="item.details.old_values['Is Paid']">
+            <strong>Is Paid:</strong>  {{ item.details.old_values['Is Paid']}}; 
+          </p>
+          <p v-if="item.details.old_values['Author ID']">
+            <strong>Author ID:</strong>  {{ item.details.old_values['Author ID']}}; 
+          </p>
+          <p v-if="item.details.old_values['ID']">
+            <strong>ID:</strong>  {{ item.details.old_values['ID']}}; 
+          </p>
+          <p v-if="item.details.old_values['Payment ID']">
+            <strong>Payment ID:</strong>  {{ item.details.old_values['Payment ID']}}; 
+          </p>
           </div> 
         </div>
       </div>
@@ -35,25 +64,86 @@ export default {
         <h1>NEW</h1>
         <div v-for="(item) of myjson" :key="item">
           <div ref='item'>
-            <p>
-             ID: {{ item.details.new_values.ID}} 
-            </p>
-            <p>
-             Amount: {{ item.details.new_values.Amount}} 
-            </p>
-            <p>
-             Start date: {{ item.details.new_values['Start date']}} 
-            </p>
-            <p>
-             End date: {{ item.details.new_values['End date']}} 
-            </p>
-            <p>
-             Comment: {{ item.details.new_values['Comment']}} 
-            </p>
-            <p>
-             Address: {{ item.details.new_values['Address']}} 
-            </p>
+            <p v-if="item.details.new_values['Start date']">
+            <strong>Start date:</strong>  {{ item.details.new_values['Start date']}}; 
+          </p>
+          <p v-if="item.details.new_values['End date']">
+            <strong>End date:</strong>  {{ item.details.new_values['End date']}}; 
+          </p>
+          <p v-if="item.details.new_values['Amount'] ">
+            <strong>Amount:</strong>  {{ item.details.new_values['Amount']}}; 
+          </p>
+          <p v-if="item.details.new_values['Comment']">
+            <strong>Comment:</strong>  {{ item.details.new_values['Comment']}}; 
+          </p>
+          <p v-if="item.details.new_values['Car ID']">
+            <strong>Car ID:</strong>  {{ item.details.new_values['Car ID']}}; 
+          </p>
+          <p v-if="item.details.new_values['Collateral amount']">
+            <strong>Collateral amount:</strong>  {{ item.details.new_values['Collateral amount']}}; 
+          </p>
+          <p v-if="item.details.new_values['Address']">
+            <strong>Address:</strong>  {{ item.details.new_values['Address']}}; 
+          </p>
+          <p v-if="item.details.new_values['Client ID']">
+            <strong>Client ID:</strong>  {{ item.details.new_values['Client ID']}}; 
+          </p>
+          <p v-if="item.details.new_values['Is Paid']">
+            <strong>Is Paid:</strong>  {{ item.details.new_values['Is Paid']}}; 
+          </p>
+          <p v-if="item.details.new_values['Author ID']">
+            <strong>Author ID:</strong>  {{ item.details.new_values['Author ID']}}; 
+          </p>
+          <p v-if="item.details.new_values['ID']">
+            <strong>ID:</strong>  {{ item.details.new_values['ID']}}; 
+          </p>
+          <p v-if="item.details.new_values['Payment ID']">
+            <strong>Payment ID:</strong>  {{ item.details.new_values['Payment ID']}}; 
+          </p>
           </div> 
+        </div>
+      </div>
+    </div>
+
+    <div class="final__data">
+      <div v-for="item of myjson" :key="item"> 
+        <div class="items">
+          <p v-if="item.details.new_values['Start date']">
+            <strong>Start date:</strong>  {{ item.details.new_values['Start date']}}; 
+          </p>
+          <p v-if="item.details.new_values['End date']">
+            <strong>End date:</strong>  {{ item.details.new_values['End date']}}; 
+          </p>
+          <p v-if="item.details.new_values['Amount'] ">
+            <strong>Amount:</strong>  {{ item.details.new_values['Amount']}}; 
+          </p>
+          <p v-if="item.details.new_values['Comment']">
+            <strong>Comment:</strong>  {{ item.details.new_values['Comment']}}; 
+          </p>
+          <p v-if="item.details.new_values['Car ID']">
+            <strong>Car ID:</strong>  {{ item.details.new_values['Car ID']}}; 
+          </p>
+          <p v-if="item.details.new_values['Collateral amount']">
+            <strong>Collateral amount:</strong>  {{ item.details.new_values['Collateral amount']}}; 
+          </p>
+          <p v-if="item.details.new_values['Address']">
+            <strong>Address:</strong>  {{ item.details.new_values['Address']}}; 
+          </p>
+          <p v-if="item.details.new_values['Client ID']">
+            <strong>Client ID:</strong>  {{ item.details.new_values['Client ID']}}; 
+          </p>
+          <p v-if="item.details.new_values['Is Paid']">
+            <strong>Is Paid:</strong>  {{ item.details.new_values['Is Paid']}}; 
+          </p>
+          <p v-if="item.details.new_values['Author ID']">
+            <strong>Author ID:</strong>  {{ item.details.new_values['Author ID']}}; 
+          </p>
+          <p v-if="item.details.new_values['ID']">
+            <strong>ID:</strong>  {{ item.details.new_values['ID']}}; 
+          </p>
+          <p v-if="item.details.new_values['Payment ID']">
+            <strong>Payment ID:</strong>  {{ item.details.new_values['Payment ID']}}; 
+          </p>
         </div>
       </div>
     </div>
@@ -66,13 +156,21 @@ h1{
 }
 .old_data{
   margin-right: 20px;
+  border-right: 2px solid red;
 }
-
+.items{
+  border: 1px solid grey;
+}
 .container{
   max-width: 1200px;
   margin: 0 auto;
   display: grid;
   grid-template-columns: 1fr 1fr;
+}
+.final__data{
+  text-align: center;
+  max-width: 1000px;
+  margin: 0 auto;
 }
 p{
   margin: 0;
